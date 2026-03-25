e49598232b95045f455ea6fde6a73ce76a5202ed
Parth <parthjb003@gmail.com>
2026-03-25 22:11:43 +0530

    Blink UI second screen

--- lib/repository/screens/login/loginscreen.dart
+++ lib/repository/screens/login/loginscreen.dart
@@ -29,7 +29,62 @@ class LoginScreen extends StatelessWidget {
           Uihelper.CustomImage(img: "image2.png"),
           SizedBox(height: 20,
           ),
-          Uihelper.CustomText(text: "India’s last minute app", color: Color(0XFF000000), fontweight: FontWeight.bold, fontsize: 20,fontfamily: "bold")
+          Uihelper.CustomText(text: "India’s last minute app"
+          , color: Color(0XFF000000),
+           fontweight: FontWeight.bold,
+            fontsize: 20,
+            fontfamily: "bold"),
+          SizedBox(height: 20),
+          Card(
+            elevation: 4,
+            shape: RoundedRectangleBorder(
+               borderRadius: BorderRadiusGeometry.circular(10),
+            ),
+            child: Container(
+              height: 174,
+              width: 350,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadiusGeometry.circular(10),
+                color: Color(0XFFFFFFFF)
+              ),
+              child: Column(children: [
+                SizedBox(height: 20,),
+                Uihelper.CustomText(text: "Parth", 
+                color: Color(0XFF000000), 
+                fontweight: FontWeight.w500, 
+                fontsize: 14),
+
+                SizedBox(height: 5,),
+                Uihelper.CustomText(
+                text: "78277XXXX", 
+                color: Color(0XFF9C9C9C), 
+                fontweight: FontWeight.bold, 
+                fontsize: 14, 
+                fontfamily: "bold"),
+
+                SizedBox(height: 20,),
+                SizedBox(height: 48,width: 295,
+                child: ElevatedButton(onPressed: (){}, style: ElevatedButton.styleFrom(
+                  backgroundColor: Color(0XFFE23744),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10)
+                  )
+                ), child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center ,
+                  children: [
+                  Uihelper.CustomText(text: "Login with", 
+                  color: Color(0XFFFFFFFF), 
+                  fontweight: FontWeight.bold, 
+                  fontsize: 14, 
+                  fontfamily: "bold"),
+                  SizedBox(width: 5,),
+                  Uihelper.CustomImage(img: "image3.png")
+                ],)),
+                )
+              ],),
+            ),
+          )
+  
         ],),
       ),
     );
