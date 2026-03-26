db7d949099eb68b75901d188a56c06a9383c3be1
Parth <parthjb003@gmail.com>
2026-03-26 22:20:42 +0530

    Developing the cart screen Part 1  Blinkit UI

--- lib/repository/screens/cart/cartscreen.dart
+++ lib/repository/screens/cart/cartscreen.dart
@@ -1,12 +1,82 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_blinkit_ui/repository/widgets/uihelper.dart';
 
 class CartScreen extends StatelessWidget{
-  const CartScreen({super.key});
+  //const CartScreen({super.key});
+  TextEditingController searchController=TextEditingController();
 
   @override
   Widget build (BuildContext context){
     return Scaffold(
-      body: Center(child: Text("Cart Screen"),),
-    );
+      body: Column(children: [
+        SizedBox(height: 40,),
+        Stack(
+          children: [
+            Container(
+          height: 190,
+          width: double.infinity,
+          color: Color(0XFFF7CB45),
+          child: Column(
+            //mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              SizedBox(height: 30,),
+              Row(children: [
+                SizedBox(width: 20,),
+                Uihelper.CustomText(text: "Blinkit in", 
+                color: Color(0XFF000000), 
+                fontweight: FontWeight.bold, 
+                fontsize: 14,
+                fontfamily: "bold"),
+              ],),
+
+
+              Row(children: [
+                SizedBox(width: 20,),
+                Uihelper.CustomText(text: "16 minutes", 
+                color: Color(0XFF000000), 
+                fontweight: FontWeight.bold, 
+                fontsize: 20,
+                fontfamily: "bold"),
+              ],),
+
+              Row(children: [
+                SizedBox(width: 20,),
+                Uihelper.CustomText(text: "HOME ", 
+                color: Color(0XFF000000), 
+                fontweight: FontWeight.bold, 
+                fontsize: 14),
+                Uihelper.CustomText(text: "- Parth , Gandhinagar, Gujarat", 
+                color: Color(0XFF000000), 
+                fontweight: FontWeight.bold, 
+                fontsize: 14,
+                fontfamily: "bold")
+              ],
+              ),
+              // SizedBox(height: 20,),
+              // Uihelper.CustomTextField(controller: searchController),
+            ],
+          ),
+        ),
+        Positioned(
+          right: 20,
+          bottom: 100,
+          child: CircleAvatar(
+            radius: 15,
+            backgroundColor: Colors.white,
+            child: Icon(
+              Icons.person,
+              color: Colors.black,
+              size: 20,
+            ),
+          )
+        ),
+        Positioned(
+          bottom: 30,
+          left: 20,
+          child: Uihelper.CustomTextField(controller: searchController),)
+          ],
+        ),
+      ],
+      ),);
   }
 }
--- lib/repository/widgets/uihelper.dart
+++ lib/repository/widgets/uihelper.dart
@@ -6,7 +6,39 @@ class Uihelper {
     return Image.asset("assets/images/$img");
     
   }
-  static CustomText({required String text,required Color color,required FontWeight fontweight,String? fontfamily,required double fontsize}){
-    return  Text(text,style: TextStyle(fontSize: fontsize,fontFamily: fontfamily??"regular",fontWeight: fontweight,color: color),);
+  static CustomText(
+    {required String text,
+    required Color color,
+    required FontWeight fontweight,
+    String? fontfamily,
+    required double fontsize}){
+    return  Text(text,style: TextStyle(
+      fontSize: fontsize,
+      fontFamily: fontfamily??"regular",
+      fontWeight: fontweight,
+      color: color),
+    );
+  }
+  static CustomTextField({required TextEditingController controller}){
+    return Container(
+      height: 40,
+      width: 360,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(10),
+        color: Colors.white,
+        border: Border.all(
+          color: Color(0XFFC5C5C5)
+        )
+      ),
+      child: TextField(
+        controller: controller,
+        decoration: InputDecoration(
+          hintText: "Search 'ice-cream'",
+          prefixIcon: Image.asset("assets/images/search_icon.png"),
+          suffixIcon: Image.asset("assets/images/mic_icon.png"),
+          border: InputBorder.none
+        ),
+      ),
+    );
   }
 }
